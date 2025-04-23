7f5f2c3ec148dbaa9f2ed457120b1d6564d80380
Nat Tessema <natnaeltessema1@gmail.com>
2025-04-23 11:10:02 -0700

    update

--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+public class Product {
+    private int id;
+    private String name;
+    private double price;
 
-public class product {
+    //Constructor
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+    //Getters
+    public int getId() {
+        return this.id;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public double getPrice() {
+        return this.price;
+    }
+    //Setters
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
